6277361663497363467c27693b5e2d294d13f2e1
Nissim Ram <nissimram1812@gmail.com>
2021-06-21 15:57:19 -0400

    Update getplacename.js

--- src/Components/Map/getplacename.js
+++ src/Components/Map/getplacename.js
@@ -3,7 +3,7 @@ export default async function getPlaceName(latlng, userFilter, localityFeatureSt
 
   const LAT = latlng.lat
   const LNG = latlng.lng
-  //const apiKey = "AIzaSyDgwqGtBnrgdxp38lz0ad8NLg3nN_-3LbU"// your google apikey
+ 
 
 
   let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=${process.env.REACT_APP_G_KEY}`
@@ -13,15 +13,13 @@ export default async function getPlaceName(latlng, userFilter, localityFeatureSt
   const response = await fetch(url)
   .then(response => response.json())
   .then(data => {
-    //console.log("callback from fetch");
-    // TODO: Implement scope slider to let user decide which to select
-    if(data.results.length === 0){
-      //console.log("NO RESULTS FROM GOOGLE API")
-    }
+ 
+    
+  
     return data.results
 
   })
 
     return response;
-    //console.log(localityName, countryName, userFilter);
+    
 }
